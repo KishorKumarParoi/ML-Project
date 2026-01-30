b4467d267aaeadeb03b4b1ac98341ab23315ea65
Kishor Kumar Paroi <1703053@student.ruet.ac.bd>
2026-01-30 21:01:25 +0600

    updated code

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ HYPEN_E_DOT = "-e ."
 
 def get_requirements(file_path: str) -> List[str]:
     """
-    This function will return the list of requirements
+    This function will return the list of requirements 
     """
     requirements = []
     with open(file_path) as file_obj:
